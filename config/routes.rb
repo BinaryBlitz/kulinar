e35750f6136baa1b939b47ee3f53738b5583a93c
Dan K <itsdanya@gmail.com>
2016-08-06 21:41:50 +0900

    Categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :restaurants, only: [:index, :show]
     resource :user, only: [:show, :create]
+    resources :categories, only: [:index, :show]
+
     resources :recipes, only: [:index, :show] do
       resources :steps, only: [:index, :show]
     end
@@ -9,6 +11,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :restaurants
+    resources :categories
+
     resources :recipes do
       resources :steps, shallow: true
     end
