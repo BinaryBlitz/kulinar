0741e050bad59ee39488e5305db817b9a9b9f638
Dan Kim <itsdanya@gmail.com>
2016-10-11 09:58:13 +0300

    Add image and restaurant to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,11 @@ Admin.create!(email: 'foo@bar.com', password: 'qwerty123')
 
 user = User.create!(first_name: 'Foo', last_name: 'Bar')
 user.update(api_token: 'foobar')
+
+image = File.open(Rails.root.join('test', 'fixtures', 'files', 'blank.jpg'))
+
+restaurant = Restaurant.create!(
+  name: 'Restaurant', description: 'Description',
+  image: image, icon: image,
+  address: 'Address', phone_number: '+79998887766', site_url: 'http://google.com'
+)
