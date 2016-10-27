bb904d61557c7f517efb011930aadbaabd830fb4
SumLare <mrhard95@gmail.com>
2016-10-28 00:07:09 +0300

    Add email and password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  api_token  :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  name       :string           not null
+#  id              :integer          not null, primary key
+#  name            :string           not null
+#  api_token       :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  email           :string
+#  password_digest :string
 #
 
 class User < ApplicationRecord
@@ -15,6 +17,8 @@ class User < ApplicationRecord
   has_many :ratings, dependent: :destroy
 
   validates :name, presence: true
+  validates :email, email: true, uniqueness: { case_sensitive: false }
 
+  has_secure_password
   has_secure_token :api_token
 end
