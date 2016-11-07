10693f7ded8ca300d770a51aaff1e80887759345
SumLare <mrhard95@gmail.com>
2016-11-08 02:42:58 +0300

    Add category to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105115537) do
+ActiveRecord::Schema.define(version: 20161107211507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20161105115537) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "image",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "days", force: :cascade do |t|
@@ -79,6 +86,8 @@ ActiveRecord::Schema.define(version: 20161105115537) do
     t.datetime "updated_at",                 null: false
     t.integer  "likes_count",    default: 0
     t.integer  "dislikes_count", default: 0
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_recipes_on_category_id", using: :btree
     t.index ["restaurant_id"], name: "index_recipes_on_restaurant_id", using: :btree
   end
 
@@ -156,6 +165,7 @@ ActiveRecord::Schema.define(version: 20161105115537) do
   add_foreign_key "picks", "selections"
   add_foreign_key "ratings", "recipes"
   add_foreign_key "ratings", "users"
+  add_foreign_key "recipes", "categories"
   add_foreign_key "recipes", "restaurants"
   add_foreign_key "steps", "recipes"
   add_foreign_key "taggings", "recipes"
