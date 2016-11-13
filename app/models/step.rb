d723709459577a42b9ec4a63f94f17034c22311e
SumLare <mrhard95@gmail.com>
2016-11-13 16:46:24 +0300

    Add use image as recipe cover to step

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,15 +2,16 @@
 #
 # Table name: steps
 #
-#  id          :integer          not null, primary key
-#  name        :string           not null
-#  description :text             not null
-#  position    :integer          not null
-#  timer       :integer
-#  image       :string
-#  recipe_id   :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                        :integer          not null, primary key
+#  name                      :string           not null
+#  description               :text             not null
+#  position                  :integer          not null
+#  timer                     :integer
+#  image                     :string
+#  recipe_id                 :integer
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  use_image_as_recipe_cover :boolean          default(FALSE)
 #
 
 class Step < ApplicationRecord
@@ -18,6 +19,18 @@ class Step < ApplicationRecord
 
   validates :name, :description, presence: true
   validates :position, numericality: { greater_than: 0 }
+  validate :image_as_recipe_cover_uniqueness
+
+  scope :with_image_as_recipe_cover, -> { where(use_image_as_recipe_cover: true) }
 
   mount_uploader :image, ImageUploader
+
+  private
+
+  def image_as_recipe_cover_uniqueness
+    return unless use_image_as_recipe_cover?
+    if Step.where.not(id: id).with_image_as_recipe_cover.any?
+      errors.add(:base, 'Нельзя добавить больше 1 главной фотографии')
+    end
+  end
 end
